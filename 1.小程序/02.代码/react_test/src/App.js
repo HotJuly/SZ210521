8a66817d5b4121bade2bfc7b4f865d30c6d1b2cc
HotJuly <949588380@qq.com>
2021-10-18 16:29:35 +0800

    10.小程序数据绑定

--- 1.小程序/02.代码/react_test/src/App.js
+++ 1.小程序/02.代码/react_test/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    msg:0
+  }
+
+  buttonRef=React.createRef();
+
+  handleClick=()=>{
+    console.log('msg',this.state.msg)
+    this.setState({
+      msg:this.state.msg+1
+    })
+    console.log('msg1',this.state.msg)
+    this.setState({
+      msg:this.state.msg+1
+    })
+    console.log('msg2',this.state.msg)
+    this.setState({
+      msg:this.state.msg+1
+    })
+    console.log('msg3',this.state.msg)
+    this.setState({
+      msg:this.state.msg+1
+    })
+    console.log('msg4',this.state.msg)
+  }
+
+  handleShow(){
+    console.log('1')
+  }
+
+  handleShow2(){
+    console.log('2')
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>{this.state.msg}</h1>
+        {/* <button onClick={this.handleClick}>+1</button>
+        <button ref={this.buttonRef}>+111</button> */}
+        <button onClick={this.handleShow} ref={this.buttonRef}>show</button>
+      </div>
+    )
+  }
+
+  componentDidMount(){
+    this.buttonRef.current.onclick=this.handleShow2;
+  }
 }
+
+
+
 
 export default App;
--- 1.小程序/02.代码/wechat-init/pages/index/index.js
+++ 1.小程序/02.代码/wechat-init/pages/index/index.js
@@ -5,14 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    msg:"我是初始化的数据"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('msg',this.data.msg)
+    // this.data.msg="我是修改之后的数据"
+    this.setData({
+      msg: "我是修改之后的数据"
+    })
+    console.log('msg1', this.data.msg)
   },
 
   /**
