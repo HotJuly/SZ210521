f7b6c1a0ac4c4594cd6d10aaa903540ce5e5ff70
HotJuly <949588380@qq.com>
2021-10-19 09:34:42 +0800

    14.原生事件机制复习

--- 1.小程序/02.代码/wechat-init/pages/log/log.js
+++ 1.小程序/02.代码/wechat-init/pages/log/log.js
@@ -1,33 +1,18 @@
-// pages/index/index.js
-// 注册页面,可以调用多次,因为可以注册多个页面
+// pages/log/log.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    msg:"我是初始化的数据"
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('msg',this.data.msg)
-    // this.data.msg="我是修改之后的数据"
-    this.setData({
-      msg: "我是修改之后的数据"
-    })
-    this.setData({
-      msg: "我是修改之后的数据1"
-    })
-    this.setData({
-      msg: "我是修改之后的数据2"
-    })
-    this.setData({
-      msg: "我是修改之后的数据3"
-    })
-    console.log('msg1', this.data.msg)
+
   },
 
   /**
