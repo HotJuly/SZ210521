a3bd6108ac7d48b36c5f47c08fe20913d8aa12ae
HotJuly <949588380@qq.com>
2021-10-23 14:53:29 +0800

    57.视频页面多视频同时播放BUG解决

--- 1.小程序/02.代码/guiguyunyinyue/pages/video/video.js
+++ 1.小程序/02.代码/guiguyunyinyue/pages/video/video.js
@@ -16,9 +16,48 @@ Page({
     videoList:[]
   },
 
+  // 用于测试练习video相关API
+  testApi(){
+    // console.log('testApi')
+    // 1.创建videoContext对象
+    const videoContext = wx.createVideoContext("3180DE6BCAB0330607DCDBD64F8D174E");
+
+    // 2.通过videoContext对象调用pause方法,暂停视频播放
+    videoContext.pause();
+  },
+
+  // 用来监视video组件是否正在播放视频
+  handlePlay(event){
+    // console.log('handlePlay')
+
+    // console.log('oldVid', this.oldVid)
+
+    // 1.当某个视频开始播放,就保存当前视频的vid
+    const vid = event.currentTarget.dataset.vid;
+
+    if (this.oldVid&&this.oldVid!==vid) {
+      // 1.创建videoContext对象
+      const videoContext = wx.createVideoContext(this.oldVid);
+
+      // 2.通过videoContext对象调用pause方法,暂停视频播放
+      videoContext.pause();
+    }
+
+    this.oldVid = vid;
+
+  },
+
   // 用于请求对应的视频列表数据
   async getVideoList(){
+    // console.log('videoList')
+    this.setData({
+      videoList:[]
+    })
+
     const result2 = await req('/video/group', { id: this.data.navId });
+
+    // console.log(2)
+
     this.setData({
       videoList: result2.datas.map((item) => {
         return item.data;
@@ -27,7 +66,7 @@ Page({
   },
 
   // 用于监视用户点击导航栏操作,并实现下划线的切换效果
-  changeNavId(event){
+  async changeNavId(event){
     // console.log('changeNavId',event)
     /*
       event.currentTarget可以获取到绑定事件的事件源对象
@@ -39,7 +78,17 @@ Page({
       // navId: event.target.dataset.id
     })
 
-    this.getVideoList();
+    wx.showLoading({
+      title:"加载中..."
+    });
+
+    // console.log(1)
+
+    await this.getVideoList();
+
+    wx.hideLoading();
+
+    // console.log(3)
   },
 
   /**
