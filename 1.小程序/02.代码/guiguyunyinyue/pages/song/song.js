324bd6fcff7a19f0ad79560447ad657812cd9547
HotJuly <949588380@qq.com>
2021-10-25 11:52:07 +0800

    69.song页面歌曲播放功能实现

--- 1.小程序/02.代码/guiguyunyinyue/pages/song/song.js
+++ 1.小程序/02.代码/guiguyunyinyue/pages/song/song.js
@@ -1,4 +1,5 @@
 // pages/song/song.js
+import req from '../../utils/req.js'
 Page({
 
   /**
@@ -6,16 +7,63 @@ Page({
    */
   data: {
 
+      // 用于存储当前页面对应歌曲的详细信息
+      songObj:{},
+
+      // 用于控制当前页面的C3状态,并且记录当前页面播放状态
+      isPlay:false,
+
+      // 用于存储当前页面对应歌曲的音频链接
+      musicUrl:null
+  },
+
+  // 用于监视用户点击播放按钮操作,实现对页面的播放状态的控制
+  handlePlay(){
+    /*
+      如果当前背景音频处于播放状态,那么就要暂停音频
+      如果当前背景音频处于暂停状态,那么就要播放音频
+     */
+
+    const backgroundAudioManager = wx.getBackgroundAudioManager();
+    if(this.data.isPlay){
+      backgroundAudioManager.pause();
+    } else {
+      backgroundAudioManager.src = this.data.musicUrl;
+      backgroundAudioManager.title = this.data.songObj.name;
+    }
+
+    this.setData({
+      isPlay:!this.data.isPlay
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad:async function (options) {
+    /*
+      小程序接收路由传参的数据,需要通过onLoad的形参options获取,options是一个query对象
+     */
+
     // console.log('options', options)
     const { songId } = options;
-    console.log('songId', songId)
+    // console.log('songId', songId)
     // console.log('song', JSON.parse(song))
+
+    const result = await req('/song/detail',{ids:songId});
+    this.setData({
+      songObj:result.songs[0]
+    })
+
+    // 动态设置当前页面导航栏标题
+    wx.setNavigationBarTitle({
+      title: this.data.songObj.name
+    })
+
+    const result1 = await req('/song/url', { id: songId });
+    this.setData({
+      musicUrl: result1.data[0].url
+    })
   },
 
   /**
