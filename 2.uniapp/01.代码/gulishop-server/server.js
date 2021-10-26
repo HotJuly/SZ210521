015391d5b581f541844c527ef5b9e88b8d9d1821
HotJuly <949588380@qq.com>
2021-10-26 16:54:53 +0800

    08.koa服务器搭建

---
+++ 2.uniapp/01.代码/gulishop-server/server.js
@@ -0,0 +1,46 @@
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+
+// 1.创建服务器应用实例对象
+// const app = express()
+const app = new Koa();
+
+// 3.创建路由
+// 3.1创建路由器实例对象
+const router = new KoaRouter();
+
+// 3.2声明使用中间件,告知koa注册所有的路由
+/*
+	中间件本质是一个函数
+		该函数具有next方法,可以执行下一个中间件函数
+
+*/
+app.use(router.routes());
+
+//3.3注册路由
+/*
+	express中路由回调函数接收参数:
+		1.request->请求报文对象
+		2.response->响应报文对象
+		3.next->执行下一个中间件
+		
+	koa中路由回调函数接收参数:
+		1.ctx->request+response->请求报文对象+响应报文对象
+			返回数据的方法:ctx.body=需要返回的数据
+		2.next->执行下一个中间件
+
+*/
+router.get('/test',function(ctx,next){
+	console.log('/test success')
+	ctx.body="/test success1"
+})
+
+
+//2.将服务器应用实例运行到指定端口,并进行监视
+app.listen(3000,function(error){
+	if(error){
+		console.log('服务器运行失败:',error)
+	}else{
+		console.log('服务器运行成功,地址为:http://localhost:3000')
+	}
+})
