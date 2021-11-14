bdca15587dba7b7956d22d67f42cd4fce1bb3ea9
HotJuly <949588380@qq.com>
2021-11-14 09:21:15 +0800

    01.TS介绍

---
+++ 4.TypeScript/02.代码/demo/index.ts
@@ -0,0 +1,7 @@
+function greeter (person) {
+    return 'Hello, ' + person
+  }
+  
+  let user = 'Yee12'
+  
+  console.log(greeter(user))
---
+++ 4.TypeScript/02.代码/demo/js/index.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return 'Hello, ' + person;
+}
+var user = 'Yee12';
+console.log(greeter(user));
